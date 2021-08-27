9bd503d5c591acb2c0f05ad0181fe21769db0952
jihoon Lee <dkekd9@naver.com>
2021-08-28 00:09:04 +0900

    Exception Handler

--- src/main/java/com/example/restfulwebservice/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/restfulwebservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.restfulwebservice.exception;
 
+import com.example.restfulwebservice.user.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -19,5 +20,11 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
     public  final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request){
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),ex.getMessage(),request.getDescription(false));
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public  final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),ex.getMessage(),request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 }
